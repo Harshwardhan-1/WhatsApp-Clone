5261ecbdbbe51f44e4b578d6858c908f888ffa00
harsh <harshwardhany87@gmail.com>
2026-06-16 10:00:03 +0530

    add chat model for message

---
+++ server/src/models/chat.model.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import {Document,Types} from 'mongoose';
+
+export interface IChat extends Document{
+    senderId:string,
+    receiverId:string,
+    message:string,
+    fileUrl?:string,
+    isSeen:boolean,
+    isDelivered:boolean,
+    createdAt:Date,
+    updatedAt:Date,
+}
+
+
+
+const chatSchema=new mongoose.Schema<IChat>({
+    senderId:{
+        type:String,
+        ref:"user",
+        required:[true,'senderId is missing'],
+    },
+    receiverId:{
+        type:String,
+        ref:"user",
+        required:[true,'receiverId is missing'],
+    },
+    message:{
+        type:String,
+        required:[true,'message field cant be empty '],
+        trim:true,   
+        minLength:[1,'atleast have 1 characters'],
+    },
+    fileUrl:{
+        type:String,
+        default:null
+    },
+    isSeen:{
+        type:Boolean,
+        default:false,
+    },
+    isDelivered:{
+        type:Boolean,
+        default:false,
+    }
+},
+{timestamps:true},
+)
+
+
+export const Chat=mongoose.model<IChat>("users chat",chatSchema);
