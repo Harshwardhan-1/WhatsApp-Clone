7f24d2224af48bcef43e7444851f661602124b05
harsh <harshwardhany87@gmail.com>
2026-06-16 23:13:39 +0530

    call api for prev message

--- client/src/hooks/use.chatTalk.ts
+++ client/src/hooks/use.chatTalk.ts
@@ -1,9 +1,45 @@
 //message hook convey user message message to backend
+import { useState,useEffect } from "react";
 import { socket } from "../utils/socket";
-export function chatTalk(){
+import { PrevMsg } from "../services/chat.service";
+
+
+interface MessageItem{
+    senderId:string,
+    receiverId:string,
+    message:string,
+    createdAt:Date,
+    updatedAt:Date,
+}
+interface Message{
+    senderId:string,
+    receiverId:string,
+    message:string,
+    createdAt:Date,
+    updatedAt:Date,
+}
+export function ChatTalk(){
+    const {messages}=PrevMsg();
+    const [message,setMessage]=useState<MessageItem[]>([]);
+
+
+
+
+    useEffect(()=>{
+        socket.on('receive_message',(data:Message)=>{
+            setMessage(prev=>[...prev,{senderId:data.senderId,receiverId:data.receiverId,message:data.message,createdAt:data.createdAt,updatedAt:data.updatedAt}]);
+        });
+        return()=>{
+         socket.off("receive_message");
+        };
+    },[]);
+    const allMessages=[...messages,...message];
+
+
+
     const userMessage=(data:{senderId:string,receiverId:string,msg:string})=>{
         if(!data.senderId || !data.receiverId)return;
         socket.emit("send_message",data);
     };
-    return {userMessage};
+    return {userMessage,allMessages};
 }
