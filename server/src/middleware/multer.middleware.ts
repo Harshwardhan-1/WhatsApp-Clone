0aecc015af08db1727965d40b70c899363990fe5
harsh <harshwardhany87@gmail.com>
2026-06-18 11:45:09 +0530

    add multer middleware and install packages

---
+++ server/src/middleware/multer.middleware.ts
@@ -0,0 +1,41 @@
+import {Request,Response,NextFunction} from 'express';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+import { mime } from 'zod';
+
+
+const uploaddir=path.join(process.cwd(),"uploads");
+if(!fs.existsSync(uploaddir)){
+    fs.mkdirSync(uploaddir,{recursive:true});
+}
+
+
+
+//_req means we find this parametre but it has no use here
+const storage=multer.diskStorage({
+    destination:(_req,file,cb)=>{
+        cb(null,uploaddir);
+    },
+    filename:(_req,file,cb)=>{
+        const uniqueName=`${Date.now()}+'-'+${Math.round(Math.random()*1e9)}+${path.extname(file.originalname)}`;
+        cb(null,uniqueName);
+    }
+})
+
+
+//fileFilter give the type to us 
+const fileFilter:multer.Options['fileFilter']=(_req,file,cb)=>{
+    cb(null,true);
+}
+
+
+
+//max 50 mb not more than 50 mb
+export const upload=({
+    storage,
+    fileFilter,
+    limits:{
+        fileSize:50*1024*1024
+    },
+})
