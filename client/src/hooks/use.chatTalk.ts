08ff56250e950d10cf468e910ca43c16fbb057bb
harsh <harshwardhany87@gmail.com>
2026-07-04 12:28:05 +0530

    show instant update of message to user whether it is seen,delivered,sent

--- client/src/hooks/use.chatTalk.ts
+++ client/src/hooks/use.chatTalk.ts
@@ -11,6 +11,9 @@ interface MessageItem{
     senderId:string,
     receiverId:string,
     message:string,
+    IsSend:boolean,
+    isDelivered:boolean,
+    isSeen:boolean,
     createdAt:Date,
     updatedAt:Date,
 }
@@ -19,6 +22,9 @@ export interface Message{
     senderId:string,
     receiverId:string,
     message:string,
+    IsSend:boolean,
+    isDelivered:boolean,
+    isSeen:boolean,
     createdAt:Date,
     updatedAt:Date,
 }
@@ -72,6 +78,32 @@ export function ChatTalk(){
     const handleEdit=(data:{messageId:string,senderId:string,receiverId:string,msg:string})=>{
         setAllMessages(prev=>prev.map(msg=>msg._id === data.messageId? { ...msg, message: data.msg }:msg)
     );}
+
+
+    const handleIsSend=(data:{messageId:string,senderId:string,receiverId:string,isSend:boolean})=>{
+        setAllMessages(prev=>prev.map(msg=>msg._id===data.messageId?{...msg,IsSend:data.isSend}:msg));
+    }
+
+    const handleIsDelivered=(data:{messageId:string,senderId:string,receiverId:string,isDelivered:boolean})=>{
+        setAllMessages(prev=>prev.map(msg=>msg._id===data.messageId?{...msg,isDelivered:data.isDelivered}:msg));
+    }
+
+
+    const handleIsSeen=(data:{messageId:string,senderId:string,receiverId:string,isSeen:boolean})=>{
+        setAllMessages(prev=>prev.map(msg=>msg._id===data.messageId?{...msg,isSeen:data.isSeen}:msg));
+    }
+
+
+    const handleIsMarkSeen=(data:{messageId:string[],isSeen:boolean})=>{
+      setAllMessages(prev=>prev.map(msg=>data.messageId.includes(msg._id)?{...msg,isSeen:data.isSeen}:msg));
+    }
+
+    
+    const handleIsDeliveredMark=(data:{messageId:string[],isDeliverd:boolean})=>{
+        setAllMessages(prev=>prev.map(msg=>data.messageId.includes(msg._id)?{...msg, isDelivered: data.isDeliverd}:msg));
+    }
+
+
  
     useEffect(()=>{
         //calling old message
@@ -94,6 +126,11 @@ export function ChatTalk(){
         socket.on("deleted_everyone",handleDeleteEveryone);
         socket.on("delete",handleDelete);
         socket.on("message_edited",handleEdit);
+        socket.on("isSend",handleIsSend);
+        socket.on("isDelivered",handleIsDelivered);
+        socket.on("real_time_isSeen",handleIsSeen);
+        socket.on("isSeenStatus",handleIsMarkSeen);
+        socket.on("isDelivered_mark",handleIsDeliveredMark);
         return()=>{
          socket.off("receive_message");
          socket.off("trigger_status",handleStatus);
@@ -103,6 +140,11 @@ export function ChatTalk(){
          socket.off("deleted_everyone",handleDeleteEveryone);
          socket.off("delete",handleDelete);
          socket.off("message_edited",handleEdit);
+         socket.off("isSend",handleIsSend);
+         socket.off("isDelivered",handleIsDelivered);
+         socket.off("real_time_isSeen",handleIsSeen);
+         socket.off("isSeenStatus",handleIsMarkSeen);
+         socket.off("isDelivered_mark",handleIsDeliveredMark);
         };
     },[locadata?._id,senderId,receiverId]);
 
@@ -119,5 +161,14 @@ export function ChatTalk(){
     const userpresence=(receiverId:string)=>{
         socket.emit("check_status",receiverId);
     }
-    return {userMessage,allmessages,userpresence,status,presence};
+    const activeChats=(data:{senderId:string,receiverId:string})=>{
+        socket.emit("active_user",{senderId:data.senderId,receiverId:data.receiverId});
+    }
+    const notActiveChats=(data:{senderId:string,receiverId:string})=>{
+        socket.emit("not_active_user",{senderId:data.senderId,receiverId:data.receiverId});
+    }
+    const user_open_chat=(data:{senderId:string,receiverId:string})=>{
+        socket.emit("user_open_chat",data);
+    }
+    return {userMessage,allmessages,userpresence,status,presence,activeChats,notActiveChats,user_open_chat};
 }
