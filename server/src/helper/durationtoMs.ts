6d2b6527d4bb0622bede258ceecd6b1cc2c51b18
Harshwardhan Yadav <harshwardhany87@gmail.com>
2026-08-11 21:44:59 +0530

    minor fix

--- server/src/helper/durationtoMs.ts
+++ server/src/helper/durationtoMs.ts
@@ -1,7 +1,7 @@
 export const durationtoMs=(duration:string):number|null=>{
     switch (duration){
         case "24hrs":
-            return 10000;
+            return 24*60*60*1000;
         case "7days":
             return 7*24*60*60*1000;
         case "90days":
