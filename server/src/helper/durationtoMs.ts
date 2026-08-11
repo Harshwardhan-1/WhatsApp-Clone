ee1556e4d51f0eebd7de38b8a9877974b4fb4d71
Harshwardhan Yadav <harshwardhany87@gmail.com>
2026-08-11 21:43:32 +0530

    add helper function to convert duration to ms

---
+++ server/src/helper/durationtoMs.ts
@@ -0,0 +1,11 @@
+export const durationtoMs=(duration:string):number|null=>{
+    switch (duration){
+        case "24hrs":
+            return 10000;
+        case "7days":
+            return 7*24*60*60*1000;
+        case "90days":
+            return 90*24*60*60*1000;
+        default: return null;
+    }
+}
